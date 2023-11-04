37c5750186cdbc4b9e3eaf5fa9f5678eb2ff2b78
DiegoBauti <diego.bautistamlp@gmail.com>
2023-11-04 01:10:55 -0500

    U

--- src/main/java/ong/controlador/ServletDonacionEmail.java
+++ src/main/java/ong/controlador/ServletDonacionEmail.java
@@ -1,6 +1,8 @@
 package ong.controlador;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,18 +14,40 @@ import ong.utils.JavaEmail;
 @WebServlet("/ServletDonacionEmail")
 public class ServletDonacionEmail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	String codigo=JavaEmail.GenerarCodigo();
+	
     public ServletDonacionEmail() {
         super();
     }
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String receptor=request.getParameter("correo-receptor");
+		String tipo=request.getParameter("tipo");
+		if(tipo.equals("tipo1")){
+			enviarEmail(request, response);
+		}else if(tipo.equals("tipo2")) {
+			confirmar(request,response);
+		}
+	}
+
+	private void confirmar(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String campoConfirmacion=request.getParameter("codigo");
+		
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		if(campoConfirmacion.equals(codigo)) {
+			out.print("valido");
+		}else {
+			out.print("invalido");
+		}		
+	}
+
+	private void enviarEmail(HttpServletRequest request, HttpServletResponse response) {
+		String receptor=request.getParameter("correo");
 		try {
-			//podría utilizar jwt
-			JavaEmail.enviarMail(receptor,"De");
+			
+			JavaEmail.enviarMail(receptor,codigo);
+			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
